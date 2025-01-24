81563ec3d9e8e7ce108799cb8acbc829550f3ffd
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2025-01-24 18:37:08 +0530

    Update MapView.jsx

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "../styles/MapView.css";
 
-function MapView() {
+function MapView({ location = "Chandigarh", zoom = 13 }) {
+  const mapSrc = `https://maps.google.com/maps?q=${encodeURIComponent(location)}&t=&z=${zoom}&ie=UTF8&iwloc=&output=embed`;
+
   return (
     <div className="map-view">
-      {/* <h3 className="map-title">Map View</h3> */}
       <iframe
         title="Map"
-        src="https://maps.google.com/maps?q=Chandigarh&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        src={mapSrc}
         className="map-iframe"
+        allowFullScreen
+        loading="lazy"
       ></iframe>
     </div>
   );
